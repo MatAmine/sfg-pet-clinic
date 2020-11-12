c4f2dcfa229dac1ab563634e44e5b2da73260acc
MatAmine <mattar.amine@gmail.com>
2020-11-12 01:41:16 +0100

    [course 150 completed] ownJpaService created with its profile

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,6 +4,12 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
+    List<Owner> findAll();
 }
